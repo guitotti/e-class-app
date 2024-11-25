b80ef57106f52f20be376c8163a4611baa56cd5c
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-11-24 21:46:05 -0300

    feat: create StudentCard component

---
+++ src/components/StudentCard/index.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+import styles from "./StudentCard.module.css";
+import { BsPersonFill } from "react-icons/bs";
+
+function StudentCard({
+  id,
+  name,
+  schedule,
+  description,
+  pendingTask,
+  pendingCorrection,
+}) {
+
+  const handleClick = () => {
+    console.log("Clicou no estudante " + id);
+  };
+
+  return (
+    <div className={styles.studentCard} onClick={handleClick}>
+      <div className={styles.studentInfo}>
+        <div className={styles.studentAvatar}>
+          <BsPersonFill />
+        </div>
+        <div>
+          <p className={styles.studentName}>{name}</p>
+          <p><strong>Horário de aula:</strong> {schedule}</p>
+        </div>
+      </div>
+      <div className={styles.studentDescription}>
+        <p>
+          <strong>{description}</strong>
+        </p>
+        <p className={styles.status}>
+          <span><span className={styles.task}>{pendingTask}</span>{" tarefa pendente"}</span>
+          <span><span className={styles.correction}>{pendingCorrection}</span>{" correção pendente"}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default StudentCard;
