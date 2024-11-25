29be8155dba1efdb7e257357e9f937c147a8a72c
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-11-25 00:15:12 -0300

    feat: add Router functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import LoginTeacher from "./pages/LoginTeacher";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/LoginTeacher";
 import LoginStudent from "./pages/LoginStudent";
 import Register from "./pages/Register";
+import StudentTaskList from "./pages/StudentTaskList";
+import StudentsList from "./pages/StudentsList";
 
 function App() {
   return (
-    // <LoginTeacher />
-    <LoginStudent />
-    // <Register/>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/login/student" element={<LoginStudent/>} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/students" element={<StudentsList />} />
+        <Route path="/student/tasks" element={<StudentTaskList />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
