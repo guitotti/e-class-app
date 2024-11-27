b56af0d4c4b4538d4997591eba36be815430aaa6
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-11-27 02:06:46 -0300

    feat: update routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import LoginStudent from "./pages/LoginStudent";
 import Register from "./pages/Register";
 import StudentTaskList from "./pages/StudentTaskList";
 import StudentsList from "./pages/StudentsList";
+import StudentPage from "./pages/StudentPage";
+import RegisterStudent from "./pages/RegisterStudent";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="*" element={<Login />} />
         <Route path="/login" element={<Login/>} />
         <Route path="/login/student" element={<LoginStudent/>} />
         <Route path="/register" element={<Register />} />
         <Route path="/students" element={<StudentsList />} />
         <Route path="/student/tasks" element={<StudentTaskList />} />
+        <Route path="/student" element={<StudentPage />} />
+        <Route path="/register/student" element={<RegisterStudent />} />
       </Routes>
     </Router>
   );
