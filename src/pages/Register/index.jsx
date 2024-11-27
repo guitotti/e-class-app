b51cc882fb99ad26d90f09bb0adfa467561cb14e
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-11-27 02:03:38 -0300

    feat: add axios request in register page

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,6 +1,36 @@
+import axios from "axios";
 import styles from "./Register.module.css";
+import { useState } from "react";
 
 function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const fetchData = async () => {
+      try {
+        await axios.post('http://localhost:3000/teacher', {
+          name: 'Fred',
+          email: email,
+          password: password,
+        })
+        .then(function (request) {
+          console.log(request);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();  
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className={styles.registerPage}>
       <div className={styles.logoSection}>
@@ -16,15 +46,15 @@ function Register() {
           <form>
             <div className={styles.inputGroup}>
               <label htmlFor="email">E-mail</label>
-              <input type="email" id="email" name="email" />
+              <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
             </div>
 
             <div className={styles.inputGroup}>
               <label htmlFor="password">Senha</label>
-              <input type="password" id="password" name="password" />
+              <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
             </div>
           </form>
-          <button type="submit">Entrar</button>
+          <button className={styles.registerButton} type="submit" onClick={handleSubmit}>Cadastrar</button>
           <a href="/login" className={styles.registerLink}>
             Já possui uma conta? Fazer login
           </a>
