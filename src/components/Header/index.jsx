85f243bb235943ddeee007144797f86da6fd9770
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-11-30 02:14:48 -0300

    feat: add handle logout function

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable react/prop-types */
+import { useNavigate } from "react-router-dom";
 import styles from "./Header.module.css";
 import { IoPower } from "react-icons/io5";
 
-
 function Header({ title }) {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
   return (
     <div className={styles.header}>
       <h2>{title}</h2>
-      <input type="text" placeholder="Pesquisar por nome" disabled/>
-      <button className={styles.logoutButton}><IoPower /></button>
+      <input type="text" placeholder="Pesquisar por nome" disabled />
+      <button className={styles.logoutButton} onClick={handleLogout}>
+        <IoPower />
+      </button>
     </div>
   );
 }
